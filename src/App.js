3930206d67f611fd1ae91c8dc43e2a2bde073546
tihonnazarov <139996570+tihonnazarov@users.noreply.github.com>
2023-08-05 05:00:14 +0600

    third commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 
-
 function App() {
   const [tasks, setTasks] = useState([]);
   const [taskText, setTaskText] = useState('');
@@ -28,22 +27,24 @@ function App() {
   };
 
   return (
+
       <div className="App m-auto w-1/4 h-auto mt-10 p-10 bg-black bg-opacity-30 rounded-md">
-        <h1 className="gap-10 bg-gray-700 rounded-2xl flex justify-center p-4 border-2 border-black">Список задач <svg
+        <h1 className="gap-10 bg-purple-500 rounded-2xl flex justify-center p-4 border-2 border-black">Список задач <svg
             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
             className="w-6 h-6">
           <path stroke-linecap="round" stroke-linejoin="round"
                 d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 010 3.75H5.625a1.875 1.875 0 010-3.75z"/>
         </svg>
         </h1>
+
         <div className="mt-10 flex gap-10">
-          <input className="bg-gray-700 rounded border-black border-2 placeholder-black placeholder:text-center outline-0"
+          <input className="bg-purple-500 rounded border-black border-2 placeholder-black placeholder:text-center outline-0"
               type="text"
               placeholder="Введите задачу"
               value={taskText}
               onChange={(e) => setTaskText(e.target.value)}
           />
-          <button className="p-2 rounded-2xl bg-gray-700 border-2 border-black hover:bg-gray-600" onClick={addTask}>
+          <button className="p-2 rounded-2xl opacity:bg border-2 border-black hover:bg-purple-500 transition delay-150 duration-300 ease-in-out" onClick={addTask}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                  stroke="currentColor" className="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15"/>
@@ -56,7 +57,7 @@ function App() {
                   key={index}
                   style={{ textDecoration: task.completed ? 'line-through' : 'none' }}
               >
-            <span className="hover:bg-gray-600 bg-gray-700 p-2 rounded-2xl border-2 border-black inline-flex gap-5 whitespace-nowrap" onClick={() => toggleTaskCompletion(index)}>
+            <span className="hover:bg-purple-400 bg-purple-500 transition delay-150 duration-300 ease-in-out p-2 rounded-2xl border-2 border-black inline-flex gap-5 whitespace-nowrap" onClick={() => toggleTaskCompletion(index)}>
               {task.text}
               <svg
                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -64,7 +65,7 @@ function App() {
                    d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zM12 2.25V4.5m5.834.166l-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243l-1.59-1.59"/>
               </svg>
             </span>
-                <button className="p-2 rounded-2xl bg-gray-700 border-2 border-black hover:bg-gray-600" onClick={() => removeTask(index)}>
+                <button className="p-2 rounded-2xl opacity:bg border-2 border-black hover:bg-purple-500 transition delay-150 duration-300 ease-in-out" onClick={() => removeTask(index)}>
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                        stroke="currentColor" className="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round"
